5545930c04caf9bb0e155caa9cf93ec0a6f978b7
Sai Kumar <saikumarpasupuleti497@gmail.com>
2026-03-13 17:05:00 -0500

    INtegrated User Prompt to backend Api

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 
 
 from fastapi import FastAPI
+from User import User
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Hello, World!"}
+
+@app.post("/{prompt}")
+def process_prompt(prompt):
+    response = User(prompt)
+    return {"response": response}
